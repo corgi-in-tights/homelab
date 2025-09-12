a0ca0683aedddb08eff8c7fb0b5a5827578d7f72
corgi-in-tights <59304120+corgi-in-tights@users.noreply.github.com>
2025-09-11 23:27:37 -0400

    move sops out of the cluster (its all a bit jank)

--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,3 @@
 CURR_DIR=$(dirname "$0")
 FILE_PATH="${1:?Please provide a file path relative to the repo root}"
-sops --config "$CURR_DIR/clusters/stable/.sops.yaml" -e --in-place "$CURR_DIR/$FILE_PATH"
+sops -e --in-place "$CURR_DIR/$FILE_PATH"
