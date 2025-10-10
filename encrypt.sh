a265f8e7167a6e5d11a4fb7b015d01ee40aa81ba
corgi-in-tights <59304120+corgi-in-tights@users.noreply.github.com>
2025-10-10 13:11:10 -0400

    i think the issue was that the key was too short

--- encrypt.sh
+++ encrypt.sh
@@ -2,12 +2,3 @@ CURR_DIR=$(dirname "$0")
 FILE_PATH="${1:?Please provide a file path relative to the repo root}"
 sops -e --in-place "$CURR_DIR/$FILE_PATH"
 
-
-# Grab the release secret
-kubectl -n auth get secret sh.helm.release.v1.authentik.v1 \
-  -o jsonpath='{.data.release}' | base64 -d > release.protobuf
-
-# Use helm to decode
-helm get values authentik -n auth
-helm get manifest authentik -n auth
-asm
