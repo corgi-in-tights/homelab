8ba8e2bae42920b1029927a803702435071d478d
corgi-in-tights <59304120+corgi-in-tights@users.noreply.github.com>
2026-01-02 15:55:07 -0500

    Configure Nginx Ingress and MetalLb, add cluster-wide domain secret, temporarily remove production cluster

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,8 +1,9 @@
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <path>"
+    echo "Usage: $0 <path> [name]"
     exit 1
 fi
 
 path="$1"
+name="${2:-secrets}"
 
-sops --encrypt "${path}/secrets.cleartext.yaml" > "${path}/secrets.yaml"
+sops --encrypt "${path}/${name}.cleartext.yaml" > "${path}/${name}.yaml"
