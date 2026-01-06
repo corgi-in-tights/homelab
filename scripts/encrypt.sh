8eea32049d0fb49e56fbbced69c7e65d56090d5f
corgi-in-tights <59304120+corgi-in-tights@users.noreply.github.com>
2026-01-06 15:47:53 -0500

    Switch to onedr0p's template style

--- scripts/encrypt.sh
+++
@@ -1,9 +0,0 @@
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <path> [name]"
-    exit 1
-fi
-
-path="$1"
-name="${2:-secrets}"
-
-sops --encrypt "${path}/${name}.cleartext.yaml" > "${path}/${name}.yaml"
--- scripts/run_in_kind.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-CLUSTER_NAME="flux-e2e"
-export KUBECONFIG="$(pwd)/.kubeconfig-kind"
-
-"$@"
